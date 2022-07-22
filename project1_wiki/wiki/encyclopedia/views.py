6258c7aea3a9147cd43126534febfbea3b2834cf
hnguye24 <nguyenhenry1996@gmail.com>
2022-07-21 20:53:01 -0500

    entry page requirements

--- project1_wiki/wiki/encyclopedia/views.py
+++ project1_wiki/wiki/encyclopedia/views.py
@@ -8,3 +8,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def wiki(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(request, "encyclopedia/error.html", {
+            "error_title": "Page Not Found",
+            "error_message": f"Sorry, \"{title}\" page does not exist."
+        })
+    
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title.capitalize(),
+        "entry": entry
+    })
